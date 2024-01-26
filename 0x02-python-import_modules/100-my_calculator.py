5c175ba999c5a6ffa73bb5b3b4e3e355260d8f9b
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-01-26 20:16:30 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
-
-
 def calc():
+    from calculator_1 import add, sub, mul, div
+    import sys
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
