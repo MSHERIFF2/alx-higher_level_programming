fbc9d7141345d344978304561590f173c50840a8
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-05 16:09:20 +0100

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
-        retrieves = my_list.pop(idx)
+        retrieves = my_list[idx]
     return retrieves
