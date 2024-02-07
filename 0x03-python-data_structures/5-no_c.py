50274b345fe3aec6964466885f0c633719d110e1
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-07 10:22:15 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    result = ""
     for char in my_string:
         if char != 'c' and char !='C':
-            for i in my char:
-                print("{}".format(char[i]))
+            result += char
+    return result
