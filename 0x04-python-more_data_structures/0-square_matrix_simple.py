9ff1909c8e49f3c0af180ac1347e29e9a4f648bd
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-13 10:55:43 +0100

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def square_row(matrix=[]):
+def square_matrix_simple(matrix=[]):
     sqr = []
     for row in matrix:
         sqrs = [elem ** 2 for elem in row]
         sqr.append(sqrs)
-    return result
+    return sqr
