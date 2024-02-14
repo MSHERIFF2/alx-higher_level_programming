f50a64ffe07c7e6a478419bf3cd71d5e085f732c
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-14 10:32:43 +0100

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pythone
+
+def common_elements(set_1, set_2):
+    interset_set = []
+    for i in set_1:
+        if i in set_2:
+            interset_set.append(i)
+    return interset_set
