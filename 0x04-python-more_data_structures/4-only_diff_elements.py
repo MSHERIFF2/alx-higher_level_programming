6aa4a2a235e1043f0aaa259e0223e4fbb40617fe
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-14 11:34:35 +0100

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    compliment = ()
+    compliment = set()
     for i in set_1:
-        if i not in set_1:
+        if i not in set_2:
             compliment.add(i)
     for i in set_2:
         if i not in set_1:
