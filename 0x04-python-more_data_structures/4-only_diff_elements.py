7fbb8dc7cfd7652b06043436cc948ea889fde6f9
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-14 10:46:03 +0100

    4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    compliment = []
+    for i in set_1:
+        if i not in set_2:
+            compliment.append(i)
+    return compliment
