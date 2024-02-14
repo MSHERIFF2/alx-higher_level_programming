55d3cabbeb13f5603f773ae66495b7f0e339defa
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-02-14 10:17:20 +0100

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    if i != i :
-        for i in my_list:
-            print(i += i)
+    uniq_ele = set(my_list)
+    sum(uniq_ele)
+    print(uni_ele)
