739360029530f54087de74bb13663cfb7de63d22
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-03-23 19:30:11 +0100

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # 0-square.py by Sheriffdeen Muritala Adegoke
 """ Defines a square """
+
+
 class Square:
-    """Square class that does nothing """
+    """Square class that does nothing"""
     pass
