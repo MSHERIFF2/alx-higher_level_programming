26c99833ff7593ea3b5ccf30df5ca7dd1448e209
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2024-03-23 19:38:18 +0100

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+# 1-square.py by Sheriffdeen
+"""Defines a square"""
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    """
+    """ class square that has attributes size """
     def __init__(self, size):
         """
         the initialization function for the square class
